030933babc5a0f887331eaa7166be8f4ee98558e
JoeZlonicky <joe.zlonicky@gmail.com>
2023-10-13 14:06:34 -0700

    Added vector tests

--- src/vector3.h
+++ src/vector3.h
@@ -16,16 +16,23 @@ struct Vector3 {
         float components[3] = {0.0f};
     };
 
-    Vector3();
-    Vector3(float x, float y, float z);
+    Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+    Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
     float& operator[](int i);
     const float& operator[](int i) const;
 
     Vector3& operator*=(float s);
+    Vector3 operator*(float s) const;
     Vector3& operator/=(float s);
+    Vector3 operator/(float s) const;
+
     Vector3& operator+=(const Vector3& v);
+    Vector3 operator+(const Vector3& v) const;
     Vector3& operator-=(const Vector3&v);
+    Vector3 operator-(const Vector3& v) const;
+
+    Vector3 operator-() const;
 
     void normalize();
     [[nodiscard]] Vector3 normalized() const;
@@ -33,25 +40,72 @@ struct Vector3 {
     [[nodiscard]] float magnitude() const;
 };
 
-inline Vector3 operator*(const Vector3& v, float s) {
-    return {v.x * s, v.y * s, v.z * s};
+inline Vector3 Vector3::operator*(float s) const {
+    return {x * s, y * s, z * s};
 }
 
-inline Vector3 operator/(const Vector3& v, float s) {
+inline Vector3 Vector3::operator/(float s) const {
     s = 1.0f / s;  // More performant to only do division once and then multiply
-    return {v.x * s, v.y * s, v.z * s};
+    return {x * s, y * s, z * s};
 }
 
-inline Vector3 operator-(const Vector3& v) {
-    return {-v.x, -v.y, -v.z};
+inline Vector3 Vector3::operator-() const {
+    return {-x, -y, -z};
 }
 
-inline Vector3 operator+(const Vector3& a, const Vector3& b) {
-    return {a.x + b.x, a.y + b.y, a.z + b.z};
+inline Vector3 Vector3::operator+(const Vector3& v) const {
+    return {x + v.x, y + v.y, z + v.z};
 }
 
-inline Vector3 operator-(const Vector3& a, const Vector3& b) {
-    return {a.x - b.x, a.y - b.y, a.z - b.z};
+inline Vector3 Vector3::operator-(const Vector3& v) const {
+    return {x - v.x, y - v.y, z - v.z};
+}
+
+inline float& Vector3::operator[](int i) {
+    return components[i];
+}
+
+inline const float& Vector3::operator[](int i) const {
+    return components[i];
+}
+
+inline Vector3& Vector3::operator*=(float s) {
+    x *= s;
+    y *= s;
+    z *= s;
+    return *this;
+}
+
+inline Vector3& Vector3::operator/=(float s) {
+    s = 1.0f / s;  // More performant to only do division once and then multiply
+    *this *= s;
+    return *this;
+}
+
+inline Vector3& Vector3::operator+=(const Vector3& v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+}
+
+inline Vector3& Vector3::operator-=(const Vector3&v) {
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    return *this;
+}
+
+inline void Vector3::normalize() {
+    *this /= magnitude();
+}
+
+inline Vector3 Vector3::normalized() const {
+    return *this / magnitude();
+}
+
+inline float Vector3::magnitude() const {
+    return (std::sqrt(x * x + y * y + z * z));
 }
 
 #endif
--- tests/test_vector3.cpp
+++ tests/test_vector3.cpp
@@ -1,6 +1,80 @@
-#include <gtest/gtest.h>
+#include "vector3.h"
 
-TEST(HelloTest, BasicAssertions) {
-    EXPECT_STRNE("Hello", "World!");
-    EXPECT_EQ(7 * 6, 42);
+#include <gtest/gtest.h>
+#include <cmath>
+
+void assertVectorValues(const Vector3& v, float x, float y, float z) {
+    ASSERT_FLOAT_EQ(v.x, x);
+    ASSERT_FLOAT_EQ(v.y, y);
+    ASSERT_FLOAT_EQ(v.z, z);
+}
+
+TEST(Vector3, DefaultConstruction) {
+    Vector3 v;
+    assertVectorValues(v, 0.0f, 0.0f, 0.0f);
+}
+
+TEST(Vector3, ValueConstruction) {
+    Vector3 v {1.0f, 2.0f, 3.0f};
+    assertVectorValues(v, 1.0f, 2.0f, 3.0f);
+}
+
+TEST(Vector3, PropertyAssignment) {
+    Vector3 v;
+    v.x = 1.0f;
+    v.y = 2.0f;
+    v.z = 3.0f;
+    assertVectorValues(v, 1.0f, 2.0f, 3.0f);
+}
+
+TEST(Vector3, IndexAssignment) {
+    Vector3 v;
+    v[0] = 1.0f;
+    v[1] = 2.0f;
+    v[2] = 3.0f;
+    assertVectorValues(v, 1.0f, 2.0f, 3.0f);
+}
+
+TEST(Vector3, Addition) {
+    Vector3 v1 {1.0f, 2.0f, 3.0f};
+    Vector3 v2 {4.0f, 5.0f, 6.0f};
+    Vector3 v3 = v1 + v2;
+    assertVectorValues(v3, 5.0f, 7.0f, 9.0f);
+}
+
+TEST(Vector3, Subtraction) {
+    Vector3 v1 {1.0f, 2.0f, 3.0f};
+    Vector3 v2 {2.0f, 4.0f, 6.0f};
+    Vector3 v3 = v1 - v2;
+    assertVectorValues(v3, -1.0f, -2.0f, -3.0f);
+}
+
+TEST(Vector3, ScalarMultiplication) {
+    Vector3 v1 {1.0f, 2.0f, 3.0f};
+    Vector3 v2 = v1 * 5;
+    assertVectorValues(v2, 5.0f, 10.0f, 15.0f);
+}
+
+TEST(Vector3, ScalarDivision) {
+    Vector3 v1 {5.0f, 10.0f, 15.0f};
+    Vector3 v2 = v1 / 5.0f;
+    assertVectorValues(v2, 1.0f, 2.0f, 3.0f);
+}
+
+TEST(Vector3, Negation) {
+    Vector3 v1 {1.0f, 2.0f, 3.0f};
+    Vector3 v2 = -v1;
+    assertVectorValues(v2, -1.0f, -2.0f, -3.0f);
+}
+
+TEST(Vector3, Magnitude) {
+    Vector3 v {1.0f, 2.0f, 3.0f};
+    float expected_magnitude = std::sqrt(1.0f * 1.0f + 2.0f * 2.0f + 3.0f * 3.0f);
+    ASSERT_FLOAT_EQ(v.magnitude(), expected_magnitude);
+}
+
+TEST(Vector3, Normalize) {
+    Vector3 v {1.0f, 1.0f, 1.0f};
+    v.normalize();
+    ASSERT_FLOAT_EQ(v.magnitude(), 1.0f);
 }
