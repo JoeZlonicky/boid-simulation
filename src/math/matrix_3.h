b38bc9c3be15ab16615807f837efa965d0f7cd21
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-11-15 14:33:43 -0800

    Refactor Matrix3/4

--- src/math/matrix_3.h
+++ src/math/matrix_3.h
@@ -4,41 +4,44 @@
 #include "vector_3.h"
 
 // Implementation takes inspiration from Foundation of Game Engine Development, Volume 1: Mathematics by Eric Lengyel
-struct Matrix3 {
+// Stored in column-major order: https://en.wikipedia.org/wiki/Row-_and_column-major_order
+class Matrix3 {
+public:
     Matrix3() = default;
-    explicit Matrix3(float s) : n{{s, 0.f, 0.f},
-                                  {0.f, s, 0.f},
-                                  {0.f, 0.f, s}} {
 
-    }
+    explicit Matrix3(float diagonal);
+
     Matrix3(float n00, float n01, float n02,
             float n10, float n11, float n12,
-            float n20, float n21, float n22) :
-            n{{n00, n10, n20},
-              {n01, n11, n21},
-              {n02, n12, n22}} { }
+            float n20, float n21, float n22);
 
+    // Parameters are in matrix notation and do not correspond to the internal indices
     void set(float n00, float n01, float n02,
              float n10, float n11, float n12,
              float n20, float n21, float n22);
 
-    Vector3& getColumn(int j);
-    [[nodiscard]] const Vector3& getColumn(int j) const;
+    Vector3 &getColumn(int j);
 
-    float& operator()(int i, int j);
-    const float& operator()(int i, int j) const;
+    [[nodiscard]] const Vector3 &getColumn(int j) const;
 
-    Matrix3& operator*=(const Matrix3& b);
-    Matrix3& operator*=(float s);
+    float &operator()(int i, int j);
+
+    const float &operator()(int i, int j) const;
+
+    Matrix3 &operator*=(const Matrix3 &b);
+
+    Matrix3 &operator*=(float s);
 
 private:
     float n[3][3];
 };
 
-Matrix3 operator*(Matrix3 a, const Matrix3& b);
+Matrix3 operator*(Matrix3 a, const Matrix3 &b);
+
 Matrix3 operator*(Matrix3 m, float s);
+
 Matrix3 operator-(Matrix3 m);
 
-Vector3 operator*(const Matrix3& m, const Vector3& v);
+Vector3 operator*(const Matrix3 &m, const Vector3 &v);
 
 #endif
--- src/math/matrix_4.cpp
+++ src/math/matrix_4.cpp
@@ -3,30 +3,42 @@
 
 void Matrix4::set(float n00, float n01, float n02, float n03, float n10, float n11, float n12, float n13, float n20,
                   float n21, float n22, float n23, float n30, float n31, float n32, float n33) {
-    n[0][0] = n00; n[1][0] = n01; n[2][0] = n02; n[3][0] = n03;
-    n[0][1] = n10; n[1][1] = n11; n[2][1] = n12; n[3][1] = n13;
-    n[0][2] = n20; n[1][2] = n21;n[2][2] = n22; n[3][2] = n23;
-    n[0][3] = n30; n[1][3] = n31;n[2][3] = n32; n[3][3] = n33;
+    n[0][0] = n00;
+    n[1][0] = n01;
+    n[2][0] = n02;
+    n[3][0] = n03;
+    n[0][1] = n10;
+    n[1][1] = n11;
+    n[2][1] = n12;
+    n[3][1] = n13;
+    n[0][2] = n20;
+    n[1][2] = n21;
+    n[2][2] = n22;
+    n[3][2] = n23;
+    n[0][3] = n30;
+    n[1][3] = n31;
+    n[2][3] = n32;
+    n[3][3] = n33;
 }
 
-Vector4& Matrix4::getColumn(int j) {
-    return (*reinterpret_cast<Vector4*>(n[j]));
+Vector4 &Matrix4::getColumn(int j) {
+    return (*reinterpret_cast<Vector4 *>(n[j]));
 }
 
-const Vector4& Matrix4::getColumn(int j) const {
-    return (*reinterpret_cast<const Vector4*>(n[j]));
+const Vector4 &Matrix4::getColumn(int j) const {
+    return (*reinterpret_cast<const Vector4 *>(n[j]));
 }
 
-float& Matrix4::operator()(int i, int j) {
+float &Matrix4::operator()(int i, int j) {
     return n[j][i];
 }
 
-const float& Matrix4::operator()(int i, int j) const {
+const float &Matrix4::operator()(int i, int j) const {
     return n[j][i];
 }
 
 Matrix4 &Matrix4::operator*=(const Matrix4 &b) {
-    const Matrix4& a = (*this);
+    const Matrix4 &a = (*this);
     set(
             a(0, 0) * b(0, 0) + a(0, 1) * b(1, 0) + a(0, 2) * b(2, 0) + a(0, 3) * b(3, 0),
             a(0, 0) * b(0, 1) + a(0, 1) * b(1, 1) + a(0, 2) * b(2, 1) + a(0, 3) * b(3, 1),
@@ -52,7 +64,7 @@ Matrix4 &Matrix4::operator*=(const Matrix4 &b) {
 }
 
 Matrix4 &Matrix4::operator*=(float s) {
-    const Matrix4& m = (*this);
+    const Matrix4 &m = (*this);
     set(
             s * m(0, 0), s * m(0, 1), s * m(0, 2), s * m(0, 3),
             s * m(1, 0), s * m(1, 1), s * m(1, 2), s * m(1, 3),
@@ -61,7 +73,7 @@ Matrix4 &Matrix4::operator*=(float s) {
     return *this;
 }
 
-Matrix4 operator*(Matrix4 a, const Matrix4& b) {
+Matrix4 operator*(Matrix4 a, const Matrix4 &b) {
     a *= b;
     return a;
 }
@@ -77,9 +89,9 @@ Matrix4 operator-(Matrix4 m) {
 
 Vector4 operator*(const Matrix4 &m, const Vector4 &v) {
     return {
-        m(0, 0) * v.x + m(0, 1) * v.y + m(0, 2) * v.z + m(0, 3) * v.w,
-        m(1, 0) * v.x + m(1, 1) * v.y + m(1, 2) * v.z + m(1, 3) * v.w,
-        m(2, 0) * v.x + m(2, 1) * v.y + m(2, 2) * v.z + m(2, 3) * v.w,
-        m(3, 0) * v.x + m(3, 1) * v.y + m(3, 2) * v.z + m(3, 3) * v.w
+            m(0, 0) * v.x + m(0, 1) * v.y + m(0, 2) * v.z + m(0, 3) * v.w,
+            m(1, 0) * v.x + m(1, 1) * v.y + m(1, 2) * v.z + m(1, 3) * v.w,
+            m(2, 0) * v.x + m(2, 1) * v.y + m(2, 2) * v.z + m(2, 3) * v.w,
+            m(3, 0) * v.x + m(3, 1) * v.y + m(3, 2) * v.z + m(3, 3) * v.w
     };
 }
